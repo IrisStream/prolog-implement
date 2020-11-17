8feb9959a0c87119fb64af3e9261ed9dab0dc9c0
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-11-17 23:14:00 +0700

    fact for teacher & deputy_head_of_department done

--- Lab 2 - Logic/script/HCMUS_Knowledgebase.pl
+++ Lab 2 - Logic/script/HCMUS_Knowledgebase.pl
@@ -1,33 +1,49 @@
-/*----------------------------------------> FACT FOR TESTING <----------------------------------------*/
+/*----------------------------------------> FACT FOR TEACHER (BB) <----------------------------------------*/
 
-student('bao long', 'major A').
-gpa('bao long', 8).
-gpa('bao', 9).
-training_point('bao long', 80).
-health_degree('bao long').
-day_number_of_volunteer('bao long', 50).
-english_degree('bao long', 'ielts').
+/*  Description
+    * có 2 bộ môn: CNPM và KHMT
+    * 2 trưởng bộ môn: Lê ngọc thành, nguyễn thị minh tuyền
+    * 2 phó bộ môn: lê hoài bắc, hồ tấn thanh 
+*/
 
-% subject in department
-subject_of('subject A', 'department A').
+male('Le Ngoc Thanh').
+male('Le Hoai Bac').
+male('Nguyen Van Khiet').
+male('Ho Tan Thanh').
+male('Nguyen Ngoc Vu').
 
-% department in faculty
-department_of('department A', 'faculty A').
+female('Nguyen Thi Ngoc Thao').
+female('Nguyen Thi Minh Tuyen').
+female('Nguyen Thi Phuong Uyen').
 
-% teacher in department
-teacher('A teacher', 'department A').
-teacher('Another teacher', 'department A').
+teacher('Le Ngoc Thanh', 'KHMT').
+teacher('Le Hoai Bac', 'KHMT').
+teacher('Nguyen Thi Ngoc Thao', 'KHMT').
+teacher('Nguyen Thi Phuong Uyen', 'KHMT').
+teacher('Ho Tan Thanh', 'CNPM').
+teacher('Nguyen Van Khiet', 'CNPM').
+teacher('Nguyen Thi Minh Tuyen', 'CNPM').
+teacher('Nguyen Ngoc Vu', 'CNPM').
 
-superior('A teacher', 'Another teacher').
+teach('Le Ngoc Thanh', '18CTT2', 'NMLT').
+teach('Le Hoai Bac', '18CTT2', 'CSAI').
+teach('Nguyen Thi Ngoc Thao', '18CTT3', 'CTDL&GT').
+teach('Nguyen Thi Phuong Uyen', '18CTT4', 'OOP').
+teach('Ho Tan Thanh', '18CTT3', 'Java').
+teach('Nguyen Thi Minh Tuyen', '18CTT2', 'NM CNPM').
+teach('Nguyen Van Khiet', '18CTT2', 'OOP').
+teach('Nguyen Ngoc Vu', '18CTT1', 'OOP').
 
-faculty_of('faculty A', 'school A').
+superior('Le Ngoc Thanh', 'Le Hoai Bac').
+superior('Le Hoai Bac', 'Nguyen Thi Ngoc Thao').
+superior('Le Hoai Bac', 'Nguyen Thi Phuong Uyen').
+superior('Nguyen Thi Minh Tuyen', 'Ho Tan Thanh').
+superior('Ho Tan Thanh', 'Nguyen Van Khiet').
+superior('Ho Tan Thanh', 'Nguyen Ngoc Vu').
 
-major_of('major A', 'faculty A').
+/*----------------------------------------> END FACT FOR TEACHER (BB) <----------------------------------------*/
 
-student_of('bao long', 'faculty A').
-student_of('bao', 'faculty A').
-
-/*----------------------------------------> RULE <----------------------------------------*/
+/*----------------------------------------> RULE OF BB <----------------------------------------*/
 
 schoolarship(Student):-
     gpa(Student, GPA),
@@ -63,12 +79,17 @@ student_of_school(Student, School):-
     major_of(Major, Faculty),
     faculty_of(Faculty, School).
 
-% deputy_head_of_department(Prof):-
+% phó bộ môn là người được trưởng bộ môn quản lý 
+% nó cũng quản lý tất cả các thằng khác trong bộ môn nhưng trong luật không nêu ra
+deputy_head_of_department(Prof):-
+    teacher(Prof, AnyDepartment),
+    head_of_department(AnotherProf),
+    superior(AnotherProf, Prof).
 
-
+% trưởng bộ môn là người không có ai quản lý
 head_of_department(Prof):-
     teacher(Prof, AnyDepartment),
-    superior(Prof, AnyOtherProf).
+    \+ superior(AnyOtherProf, Prof).
 
 graduated_type(Student, 'Gioi'):-
     gpa(Student, GPA),
@@ -90,3 +111,14 @@ graduated_type(Student, 'Yeu'):-
 
 graduate_valedictorian(Student):-
     gpa(Id, W), \+ (gpa(_, W1), W1 > W).
+
+
+best_of_faculty(Student):-
+    student_of(Student,Faculty1),
+    gpa(Student,Point), \+ (
+        student_of(S,Faculty1),
+        gpa(S,Point1),
+        Point1 > Point
+    ).
+
+/*----------------------------------------> END RULE OF BB <----------------------------------------*/
